0cc124dd2ccd14bc299188c41377430916952803
midhun7ui <harikrishnanr948@gmail.com>
2025-07-31 19:09:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showSignUp() {
+    document.getElementById("container").classList.add("move");
+}
+
+function showSignIn() {
+    document.getElementById("container").classList.remove("move");
+}
